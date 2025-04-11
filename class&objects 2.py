10a22270f08c937e827cc571cda8c9a502513cc9
Gershon Roy <gershownroy2002@gmail>
2025-04-11 11:20:55 +0530

    modified class&object abd 2

---
+++ class&objects 2.py
@@ -0,0 +1,16 @@
+class gershon:
+    name="roy"
+    number=36
+    place="madurai"
+    def about(self):
+        print("he lives in koodal nagar")
+    def college(self):
+        print(" he done his ug in klnce")
+boy1=gershon()
+print(boy1.number)
+print(boy1.about())
+
+boy2=gershon()
+print(boy2.place)
+print(boy2.college())
+print(boy2.name)
